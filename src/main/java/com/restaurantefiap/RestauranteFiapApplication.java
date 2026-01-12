145c08e24829d8848981a2512863e962a147860b
Danilo Bossanova <danilo.bossanova@hotmail.com>
2026-01-12 16:06:00 -0300

    Adicionando banner de inicialização

--- src/main/java/com/restaurantefiap/RestauranteFiapApplication.java
+++ src/main/java/com/restaurantefiap/RestauranteFiapApplication.java
@@ -1,13 +1,40 @@
 package com.restaurantefiap;
 
+import com.restaurantefiap.config.ConsoleLogUtil;
+import lombok.Value;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.event.ApplicationReadyEvent;
+import org.springframework.context.event.EventListener;
 
 @SpringBootApplication
 public class RestauranteFiapApplication {
 
+
+    private String versao = "2026/01/10";
+
     public static void main(String[] args) {
         SpringApplication.run(RestauranteFiapApplication.class, args);
+    }
+
+
+    @EventListener(ApplicationReadyEvent.class)
+    public void printStartedBanner() {
+        String labelVersao = ConsoleLogUtil.formatar("Versão", ConsoleLogUtil.Cor.AMARELO, ConsoleLogUtil.Estilo.NEGRITO);
+        String formatarVersao = ConsoleLogUtil.formatar("%s", ConsoleLogUtil.Cor.VERMELHO);
+        String poweredBy = ConsoleLogUtil.formatar("Powered by Grupo 8", ConsoleLogUtil.Cor.VERDE);
+
+        String banner =
+                "  __  __           _____       _  _______ \n" +
+                        " /_ |/_ |    /\\   |  __ \\     | ||__   __|\n" +
+                        "  | | | |   /  \\  | |  | |    | |   | |   \n" +
+                        "  | | | |  / /\\ \\ | |  | |    | |   | |   \n" +
+                        "  | | | | / ____ \\| |__| | |__| |   | |   \n" +
+                        "  |_| |_|/_/    \\_\\_____/ \\____/    |_|   \n\n" +
+                        ":: " + labelVersao + " " + formatarVersao + " ::\n" +
+                        ":: " + poweredBy + " ::\n";
+
+        System.out.printf((banner) + "%n", versao);
     }
 
 }
